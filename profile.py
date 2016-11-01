a49d08398cd9e8552fca445fc9e109d0cd8087ca
Yuliia Synytsia <lm.julia92@gmail.com>
2016-11-01 16:47:23 -0700

    modified for profile creation only

--- profile.py
+++ profile.py
@@ -1,45 +1,14 @@
 import json
 import sqlite3
-
-class Digraph:
-    def __init__(self):
-        self.temp = [] # temporarily queue
-
-    def addModif(self,keyinput): #keyinput = [key+action,value]
-        self.temp.insert(0,keyinput) #insert keyinput to a queue.
-        if len(self.temp) == 3:
-            act1 = self.temp.pop() # take away first value and store 2 others for next digraph
-            act2 = self.temp[1]
-            act3 = self.temp[0]
-            time1 = abs(float(act1[1]) - float(act2[1]))
-            time2 = abs(float(act3[1]) - float(act2[1]))
-            time = float(time1/time2)
-            #print "time 1 = ", time1
-            #print "time 2 = ", time2
-            #print "time = time1/time2 = ", time
-            key = act1[0]  + act2[0] + act3[0]
-            return key, time
-        else:
-            return None, None
-
-    def add(self,keyinput): #keyinput = [key+action,value]
-            self.temp.insert(0,keyinput) #insert keyinput to a queue.
-            if len(self.temp) == 2:
-                act1 = self.temp.pop() # take away first value and store 2 others for next digraph
-                act2 = self.temp[0]
-                time = abs(float(act1[1]) - float(act2[1]))
-                key = act1[0]  + act2[0]
-
-                return key, time
-            else:
-                return None, None
+import digraph
+import getpost
 
 class Etalon:
     def __init__(self):
         self.keylog = {} # formed digraphs
 
     def addToEtalon(self, data):
-        newDigraph = Digraph()
+        newDigraph = digraph.Digraph()
         for item in data:
             #key, value = newDigraph.addModif(item)
             key, value = newDigraph.add(item)
@@ -75,43 +44,45 @@ class Profile:
             print item[0]
             item[1].printEtalon()
 
-def getpostdata():
-    with open("postreq.json", 'r') as f:
-        postdata = f.read()
-    rawinput = json.loads(postdata)
-    username = "".join(rawinput.keys())
-    rdata = []
-    for item in rawinput[username]:
-        values = item.values()
-        keylog = values[0].split(",")
-        timelog = values[1].split(",")
-        for i in xrange(len(keylog)):
-            rdata.append([keylog[i],timelog[i]])
-    return username, rdata
+def is_table_exists(name):
+    conn = sqlite3.connect('keystroke.sqlite')
+    cur = conn.cursor()
+    name = str(name.replace(" ", ""))
+    cur.execute("SELECT * FROM sqlite_master WHERE type='table' AND name=?", (name,))
+    row = cur.fetchone()
+    cur.close()
+    if row is not None:
+        print "Username alredy exists"
+        return True
+    else:
+        return False
 
-def pushtosql(prof):
+def push_to_sql(prof):
+    table  = "".join(prof.keys())
     conn = sqlite3.connect('keystroke.sqlite')
     cur = conn.cursor()
 
-    #cur.execute('''
-    #DROP TABLE IF EXISTS Users''')
+    if is_table_exists(table):
+        return False
 
     cur.execute('''
-    CREATE TABLE IF NOT EXISTS Users (user TEXT, digraph_id INTEGER, expected_value REAL, variance REAL, frequency INTEGER)''')
-    uname = "".join(prof.keys())
+    CREATE TABLE IF NOT EXISTS ''' + table +'''(digraph_id INTEGER, expected_value REAL, variance REAL, frequency INTEGER)''')
     keys = prof["".join(prof.keys())].getkeys()
     values = prof["".join(prof.keys())].getvalues()
     for i in xrange(len(keys)):
-        cur.execute('''INSERT INTO Users (user, digraph_id, expected_value, variance, frequency)
-        VALUES ( ?, ?, ?, ?, ? )''', (uname, keys[i], values[i][0], values[i][1], values[i][2]))
+        cur.execute('''INSERT INTO ''' + table +''' (digraph_id, expected_value, variance, frequency)
+        VALUES ( ?, ?, ?, ? )''', (keys[i], values[i][0], values[i][1], values[i][2]))
     conn.commit()
+    cur.close()
+    return True
+
 
 myprofile = Profile()
 
-user, data = getpostdata()
+user, data = getpost.get_post_data()
+if not is_table_exists(user):
+    print "User already exists"
 
 user_profile = myprofile.createProfile(user, data)
-
-pushtosql(user_profile)
-
-#myprofile.printProfile()
+push_to_sql(user_profile)
+myprofile.printProfile()
--- server.py
+++ server.py
@@ -1,5 +1,4 @@
 from bottle import run, request, post
-import subprocess
 import os
 
 @post('/')
@@ -7,10 +6,9 @@ def index():
     postdata = request.body.readlines()
     with open("postreq.json", "w") as f:
         for line in postdata:
-            #print line
             f.write(line)
     os.system("python profile.py")
-    return "OK"
+    return "Thank you. Your data processed and will ready to view soon."
 
 if __name__ == "__main__":
     run(host='0.0.0.0', port=8080)
